cd0d2e4a92c5ff2f5ca970e21ee3351eaf47f97b
VINICIUS NUNES DE OLIVEIRA <22282114@CFP127ALUNO.LOCAL>
2023-02-23 11:24:00 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict'
+
+import { produtos } from "./produtos.js"
+
+// console.log(produtos[1])
+
+const precos = [10, 23, 51, 90]
+const criarCard = (produto) => {
+    const card = document.createElement('div')
+    card.classList.add('card')
+
+    const img = document.createElement('img')
+    img.classList.add('card__image')
+    img.src = `./img/${produto.image}`
+
+    const titulo = document.createElement('h5')
+    titulo.classList.add('card__title')
+    titulo.textContent = produto.name
+
+    const descricao = document.createElement('p')
+    descricao.classList.add('card__description')
+    descricao.textContent = produto.description
+
+    const preco = document.createElement('span')
+    preco.classList.add('card__price')
+    preco.textContent = produto.price
+
+    card.append(img, titulo, descricao, preco)
+
+    return card
+}
+const carregarProdutos = () => {
+    const container = document.getElementById('container')
+    const cards = produtos.map(criarCard)
+
+    container.replaceChildren(...cards)
+}
+
+carregarProdutos()
+
+
+// const limite = precos.length
+// const precosFrete = []
+// for (let i = 0; i < limite; i++) {
+//     precosFrete[i] = precos[i] + 2
+// }
+
+// console.log(precosFrete)
+// const adicionaFrete = (preco) => preco + 2
+// const precosFrete = precos.map(adicionaFrete)
+// console.log(precosFrete)
